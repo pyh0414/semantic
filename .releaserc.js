641bea996b2e95c0bec3df64861c97753a6af37a
yeonhoPark <whwlsvy12@naver.com>
2023-09-21 18:20:27 +0900

    feat: staging9

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,36 @@
-module.exports = {
-  branches: ["staging"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false,
-      },
+const { execSync } = require("child_process");
+
+function executeGitCommand(command) {
+  return execSync(command)
+    .toString("utf8")
+    .replace(/[\n\r\s]+$/, "");
+}
+
+const branch = executeGitCommand("git rev-parse --abbrev-ref HEAD");
+
+console.log(branch);
+
+if (branch === "staging") {
+  module.exports = {
+    branches: ["staging"],
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      [
+        "@semantic-release/npm",
+        {
+          npmPublish: false,
+        },
+      ],
+      "@semantic-release/github",
+      "@semantic-release/git",
     ],
-    "@semantic-release/github",
-    "@semantic-release/git",
-  ],
-};
+  };
+} else {
+  module.exports = {
+    branches: ["master"],
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+    ],
+  };
+}
