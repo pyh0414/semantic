09fd963f98741ccb547c6c6cb2240cc74d97f367
yeonhoPark <whwlsvy12@naver.com>
2023-09-21 18:13:52 +0900

    feat: staging7

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-console.log("36");
-console.log("2");
-console.log("2");
-console.log("2");
-console.log("2");
-console.log("2");
-console.log("2");
-console.log("2");
-console.log("2");
+const { execSync } = require("child_process");
+
+function executeGitCommand(command) {
+  return execSync(command)
+    .toString("utf8")
+    .replace(/[\n\r\s]+$/, "");
+}
+
+const branch = executeGitCommand("git rev-parse --abbrev-ref HEAD");
+
+console.log(process.env);
+console.log(branch);
